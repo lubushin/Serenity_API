dd18c9324fe0cda94bd2efdc8b6029f7d69cf083
Lyubov Ubushin <Lyubau1124@gmail.com>
2021-04-15 11:04:00 -0400

    Initial commit

---
+++ src/test/java/B21/github/GithubTest.java
@@ -0,0 +1,31 @@
+package B21.github;
+
+import org.junit.jupiter.api.*;
+import static io.restassured.RestAssured.*;
+public class GithubTest {
+   //https://github.com/Cybertek-B21/B21SerenityProject
+    //https://api.github.com/users/CybertekSchool
+
+    @BeforeAll
+    public static void setUp(){
+        baseURI = "https://github.com";
+
+    }
+    @Test
+    public void testGitHub(){
+        given()
+                .pathParam("user_id" , "CybertekSchool")
+                .log().all().
+        when()
+                .get("/users/{user_id}").
+        then()
+                .log().all()
+                .statusCode(200) ;
+    }
+
+   @AfterAll
+   public static void cleanUp(){
+    reset();
+
+   }
+}
